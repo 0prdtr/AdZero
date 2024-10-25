cc37d0eed05d880fee1db588e504633898723362
0prdtr <119495485+0prdtr@users.noreply.github.com>
2024-10-25 11:46:09 -0300

    algunos cambios

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ const body = document.body;
 toggleButton.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
 });
+// Neutraliza scripts que verifican la carga de anuncios
+window.addEventListener('DOMContentLoaded', (event) => {
+    // Sobrescribe ciertos métodos que usan los sitios para detectar adblock
+    const noop = () => {};
+    const overwriteDetectionMethods = () => {
+        // Métodos que frecuentemente se sobrescriben para evitar detección
+        window.AdBlockDetected = noop;
+        document.AdBlockDetected = noop;
+        window.BlockedAds = false;
+    };
+    
+    // Intento inicial para sobrescribir métodos
+    overwriteDetectionMethods();
+
+    // Vuelve a intentar después de un tiempo para scripts cargados dinámicamente
+    setTimeout(overwriteDetectionMethods, 2000);
+});
